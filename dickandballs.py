eeb938a6ca9e2ec814acd905829e6f8c8313cd2e
samridho ghosh <samriddhoghosh@protonmail.com>
2025-01-06 20:58:47 +0100

    eos properties & prior mean

--- dickandballs.py
+++ dickandballs.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import sympy as sp
 from scipy.spatial.distance import cdist
 from scipy.linalg import cho_solve, solve_triangular, cholesky
+from scipy.integrate import cumulative_simpson as cumsimp
 
 
 
@@ -58,12 +59,9 @@ class Kernel:
         r2 = cdist(x1, x2, metric='sqeuclidean')
         K = sigma ** 2 * np.exp(-0.5 * r2 / ( l ** 2))
 
-        #if x1.shape == x2.shape:
-        #    jitter = 1e-10
-        #    K += np.diag(np.full(x1.shape[0], jitter))
-        
         return K
     
+    # utilities
     def _ensure_shape(self, x):
         """Ensure that the input array is of shape (n,1)."""
         if x.ndim == 1:
@@ -73,32 +71,164 @@ class Kernel:
 
 class GP:
     # very basic, no noise for now
-    def __init__(self):
-        pass
+    """
+    Inputs: 
+    - kernel
+    - prior mean: mean function to be evaluated at x_train and x_test or a global mean
+    """
+    def __init__(self, kernel, prior_mean = None):
+        self.kernel = kernel
+        if prior_mean is not None: 
+            self.prior_mean = prior_mean
+        else:
+            self.prior_mean = 0
+        self.mean_star = None
+        self.cov_star = None
 
-    def process():
-        y = np.random.multivariate_normal(mean=mu, cov=K, size=n)
-        return y
-    
-    def fit(x1, x2, f, kernel):
 
-        kernel = kernel
-        K_11 = kernel.compute(x1)
-        K_12 = kernel.compute(x1, x2)
-        K_22 = kernel.compute(x2)
+    def fit(self, x1, x2, f, stabilise=False, jitter_value=1e-10):
+        """
+        fits the GP with the training data
+        input of training data x1 and f, test data x2, whether to stabilise with jitter and jitter value
+        increase jitter value if error like 
+        returns the mean and covariance metric
+        """
 
-        #K_11[np.diag_indices_from(K_11)] += 1e-10
+        if callable(self.prior_mean):
+            # if prior_mean is function, evaluate fx at every point
+            mean_train = self.prior_mean(x1)
+            mean_test = self.prior_mean(x2)
+        else:
+            # use the constant value
+            mean_train = self.prior_mean
+            mean_test = self.prior_mean
+        
+        K_11 = self.kernel.compute(x1)
+        K_12 = self.kernel.compute(x1, x2)
+        K_22 = self.kernel.compute(x2)
 
-        L = cholesky(K_11, lower=True)
+        if stabilise:
+            # add jitter to the diagonals, helps with numerical stability
+            K_11[np.diag_indices_from(K_11)] += jitter_value
 
-        alpha = cho_solve((L,True), f) # cho_solve does A\f (where A = LL.T) as opposed to using solve_triangular to find (L\f) and then (L.T \ (L\f))
+        try:
+            # Perform Cholesky decomposition
+            L = cholesky(K_11, lower=True)
+        except Exception as e:
+            raise ValueError("The matrix K_11 probably is not semi-positive definite. Using a jitter value, or increasing it can help with numerical stability.") from e
+
+        f_tilde = f - mean_train # setting f_tilde to have mean of zero
+
+        alpha = cho_solve((L,True), f_tilde) # cho_solve does A\f (where A = LL.T) as opposed to using solve_triangular to find (L\f) and then (L.T \ (L\f))
     
         v = solve_triangular(L, K_12, lower=True)
 
-        mean_star = K_12.T @ alpha
-        
-        
-        cov_star = K_22 - (v.T @ v)
+        self.mean_star = mean_test + (K_12.T @ alpha)
+        self.cov_star = K_22 - (v.T @ v)
 
+    def process(self,n=1, mu=None, cov=None):
+        """
+        uses covariance metric and mean to produce 1 function from the GP
+        input: n samples
+        """
+
+        if mu is None:
+            mu = self.mean_star
+        if cov is None:
+            cov = self.cov_star
+
+        if self.mean_star is None or self.cov_star is None:
+            print("Use the fit function first before producing the GP or provide mean and kernel")
+
+        y = np.random.multivariate_normal(mean=mu, cov=cov, size=n)
+        sig = np.sqrt(np.diag(self.cov_star))
+
+        return y, sig
+    
+
+class EosProperties:
+    """
+    Example usage:
+    given initial values of epsilon, p, and mu, and n:
+    n = np.array([...])  # Example input for n
+    phi = np.array([...])  # Example input for phi
+    eos = EosProperties(mu_0, epsi_0, p_0, n, phi)
+    results = eos.get_all()
+    print(results)
+    """
+    def __init__(self,  n, phi, epsi_0, p_0, mu_0):
+        self.mu_0 = mu_0
+        self.epsi_0 = epsi_0
+        self.p_0 = p_0
+        self.n = n
+        self.phi = phi
+        self.cs2 = None
+        self.mu = None
+        self.epsilon = None
+        self.pressure1 = None
+        self.pressure2 = None
+
+    def get_cs2(self):
+        """ab initio qcd paper, eq 7
+        unitless
+        """
+        self.cs2 = 1 / (np.exp(-self.phi) + 1)
+        return self.cs2
+
+    def get_mu(self):
+        """eq 8
+        mu_0 is in Mev usually so then is mu
+        """
+        integrand = self.cs2 / self.n
+        integral = cumsimp(y=integrand, x=self.n, initial=np.log(self.mu_0))
+        log_mu = np.log(self.mu_0) + integral
+
+        try:
+            # Try to compute mu
+            self.mu = np.exp(log_mu)
+        except Exception as e:
+            # Return log_mu if it doesn't work
+            print("Error in exponentiating, returning log_mu instead")
+            self.mu = log_mu
         
-        return mean_star, cov_star
+        #TODO: make sure log_mu return is handled when get_all() is called
+
+        return self.mu
+
+    def get_epsilon(self):
+        """eq 9
+        MeV * fm^-3
+        """
+        self.epsilon = self.epsi_0 + cumsimp(y=self.mu, x=self.n, initial=self.epsi_0)
+        return self.epsilon
+
+    def get_pressure1(self):
+        """below eq 9"""
+        self.pressure1 = -self.epsilon + self.mu * self.n
+        return self.pressure1
+
+    def get_pressure2(self):
+        """Hauke's eq"""
+        integrand = self.cs2 * self.mu
+        integral = cumsimp(y=integrand, x=self.n, initial=self.p_0)
+        self.pressure2 = self.p_0 + integral
+        return self.pressure2
+
+    def get_all(self):
+        """
+        get all properties in a dictionary at once
+        """
+        self.get_cs2()
+        self.get_mu()
+        self.get_epsilon()
+        self.get_pressure1()
+        self.get_pressure2()
+        # Return all computed values as a dictionary for easy access
+        return {
+            "cs2": self.cs2,
+            "mu": self.mu,
+            "epsilon": self.epsilon,
+            "pressure1": self.pressure1,
+            "pressure2": self.pressure2
+        }
+    
