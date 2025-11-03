d116b116420e3102de60bd3876634cce9176702c
samridho ghosh <samriddhoghosh@protonmail.com>
2025-11-03 15:09:07 +0100

    bash script finally works

--- gen.sh
+++ gen.sh
@@ -2,32 +2,45 @@
 
 VALIDATE=false
 
-for arg in "$@"
-do
+for arg in "$@"; do
   case $arg in
-    --validate)
-      VALIDATE=true
-      shift
-      ;;
-    *)
-      echo "Unknown option: $arg"
-      exit 1
-      ;;
+  --validate)
+    VALIDATE=true
+    shift
+    ;;
+  *)
+    echo "Unknown option: $arg"
+    exit 1
+    ;;
   esac
 done
 
 source "$(conda info --base)/etc/profile.d/conda.sh"
-conda activate thesis || { echo "Failed to activate conda environment"; exit 1; }
+conda activate thesis || {
+  echo "Failed to activate conda environment"
+  exit 1
+}
 
+cd gpr
 echo "Generating EOS samples:"
-python gpr/generate.py || { echo "Failed to execute generate samples"; exit 1; }
+python generate.py || {
+  echo "Failed to execute generate samples"
+  exit 1
+}
 
+cd ../TOV
 echo "Converting to MR/Tidal curves"
-TOV/TOV || { echo "Failed to convert to MR/Tidal curves"; exit 1; }
+./TOV || {
+  echo "Failed to convert to MR/Tidal curves"
+  exit 1
+}
 
-if ["$VALIDATE" = true]; then
-    echo "Validating samples:"
-    python gpr/validate_samples.py || { echo "Failed to Validate samples"; exit 1; }
+if [ "$VALIDATE" = true ]; then
+  echo "Validating samples:"
+  python gpr/validate_samples.py || {
+    echo "Failed to Validate samples"
+    exit 1
+  }
+fi
 
 echo "Done and dusted"
-
